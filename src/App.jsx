0578a5ebdf879c029f8acc4367dc278d5d5fc891
Elahills <158523995+Elahills@users.noreply.github.com>
2024-02-16 22:27:43 +0000

    modified to personal details

--- src/App.jsx
+++ src/App.jsx
@@ -24,15 +24,15 @@ import "./styles.css";
  * If you don't have one of the social sites listed, leave it as an empty string.
  */
 const siteProps = {
-  name: "Alexandrie Grenier",
-  title: "Web Designer & Content Creator",
-  email: "alex@example.com",
-  gitHub: "microsoft",
-  instagram: "microsoft",
-  linkedIn: "satyanadella",
+  name: "Elahills",
+  title: "IT student, Web Designer & Content Creator",
+  email: "Maryannchelu31@gmail.com",
+  gitHub: "elahills",
+  instagram: "ela_hills",
+  linkedIn: "",
   medium: "",
-  twitter: "microsoft",
-  youTube: "Code",
+  twitter: "elahills",
+  youTube: "",
 };
 
 const primaryColor = "#4E567E";
--- src/Components/About.jsx
+++ src/Components/About.jsx
@@ -16,15 +16,15 @@ import React from "react";
  * Need an image? Check out https://unsplash.com to download a image you
  * freely use on your site.
  */
-import image from "../images/motion-background.jpg";
+import image from "../images/annie-spratt-0ZPSX_mQ3xI-unsplash.jpg";
 
-const imageAltText = "purple and blue abstract background";
+const imageAltText = "green abstract aesthetic background";
 
 /**
  * Sort description that expands on your title on the Home component.
  */
 const description =
-  "I'm a UI/UX student studying at Barnett Technical University. I enjoy creating unique and simplistic user interfaces in creative ways.";
+  "I'm an IT student studying at Eastern Mediterranean University. I enjoy creating unique and simplistic user interfaces in creative ways, as well as generating content.";
 
 /**
  * List of some of skills or technologies you work on, are learning,
@@ -45,7 +45,7 @@ const skillsList = [
  * about you on a professional level.
  */
 const detailOrQuote =
-  "I am passionate about solving problems in new creative ways to drive innovation. By leveraging my UI/UX experience I continually look for new and better ways to make tech accessible by all.";
+  "I am passionate about solving problems through innovative approaches to drive progress. Continuously seeking out fresh perspectives, I aim to improve accessibility to technology for everyone.";
 
 const About = () => {
   return (
